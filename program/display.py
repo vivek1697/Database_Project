6b5e91342f32722b472e8c55184db63d0b0bc9b1
Vivek Patel <vivek.d.ptl@gmail.com>
2019-11-18 23:00:25 -0330

    display table task done

--- program/display.py
+++ program/display.py
@@ -1,7 +1,41 @@
 import ast
+import json
+import pathlib
+import os.path
+
 
 def displayTree(fname):
     return
 
 def displayTable(rel, fname):
+    # Ensure that proper path for the desired rel_att is fetched
+    path = "../data/" + rel + "/"
+    
+
+    tuple_list = []
+    content = open(os.path.join(os.path.dirname(__file__), path + "pageLink.txt"), 'r')
+    rel_page_link_list = json.loads(content.read())
+    content.close()
+    for item in rel_page_link_list:
+        content = open(os.path.join(os.path.dirname(__file__), path + item), 'r')
+        tuples_in_file = json.loads(content.read())
+        content.close()
+        for eachTuple in tuples_in_file:
+            tuple_list.append(eachTuple)
+
+    
+    if os.path.isfile(os.path.join(os.path.dirname(__file__), fname)):
+        f = open(os.path.join(os.path.dirname(__file__), fname ), "a")
+        f.write("\n" + rel + "\n")
+        f.write("\n" + json.dumps(tuple_list) + "\n") 
+        f.close()
+    else:
+        f = open(os.path.join(os.path.dirname(__file__), fname ), "w+")
+        f.write("\n" + rel + "\n")
+        f.write("\n" + json.dumps(tuple_list) + "\n")    
+        f.close
     return
+
+
+#displayTable("Products","abc.txt")
+
