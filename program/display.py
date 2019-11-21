5cdb4154f02702ec0dc41e670cb04b2f150e955e
CadmusCassidyJung <9advait@gmail.com>
2019-11-21 12:28:43 -0330

    display tree function

--- program/display.py
+++ program/display.py
@@ -5,7 +5,16 @@ import os.path
 
 
 def displayTree(fname):
-    return
+    with open(os.path.join(os.path.dirname(__file__), "../index/" + fname),"r") as rootFile:
+        content = json.loads(rootFile.read())
+        with open(os.path.join(os.path.dirname(__file__), "../index/directory.txt"),"r") as directoryFile:
+            directoryContent = json.loads(directoryFile.read())
+            treeFileName = list(directoryContent.keys())[list(directoryContent.values()).index(content)]
+            with open(os.path.join(os.path.dirname(__file__), "../treePic/" + treeFileName),"r") as treeFile:
+                tree_json = json.loads(treeFile.read())
+                print("The tree file is : "+treeFileName+"\n\nThe tree in an unordered format is ...")
+                print(json.dumps(tree_json, indent=4))
+    return treeFileName
 
 def displayTable(rel, fname):
     # Ensure that proper path for the desired rel_att is fetched
@@ -24,18 +33,19 @@ def displayTable(rel, fname):
             tuple_list.append(eachTuple)
 
     
-    if os.path.isfile(os.path.join(os.path.dirname(__file__), fname)):
-        f = open(os.path.join(os.path.dirname(__file__), fname ), "a")
+    if os.path.isfile(os.path.join(os.path.dirname(__file__), "../queryOutput/" + fname)):
+        f = open(os.path.join(os.path.dirname(__file__), "../queryOutput/" + fname ), "a")
         f.write("\n" + rel + "\n")
         f.write("\n" + json.dumps(tuple_list) + "\n") 
         f.close()
     else:
-        f = open(os.path.join(os.path.dirname(__file__), fname ), "w+")
+        f = open(os.path.join(os.path.dirname(__file__), "../queryOutput/" + fname ), "w+")
         f.write("\n" + rel + "\n")
         f.write("\n" + json.dumps(tuple_list) + "\n")    
         f.close
     return
 
 
-#displayTable("Products","abc.txt")
+# displayTable("project_select_join_join_Supply_sid_Suppliers_sid_2_pid_Products_pid_0_cost_gte_47_0_sname_pname_cost_0","queryResult.txt")
 
+# displayTree("pg94.txt")
